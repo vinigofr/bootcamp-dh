c8be6225d6522b949fd4a4508f85d38b9b9b1e94
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2022-05-30 20:30:19 -0300

    Adiciona código do exercício de QueryString - PENSE NUM TREM COMPLICADO DE FAZER DINÂMICO

--- module_three/go-web/main.go
+++ module_three/go-web/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 )
 
 type transaction struct {
@@ -16,10 +18,76 @@ type transaction struct {
 	Receiver        string  `json:"receiver"`
 }
 
-func GetAll(c *gin.Context) {
+func QueryStringFilter(c *gin.Context) {
+	data, err := ioutil.ReadFile("transactions.json")
 
-	data, err := ioutil.ReadFile("tranfdsfsd" +
-		"sactions.json")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"erro":        err.Error(),
+			"mensagem":    "Ocorreu um erro ao resgatar as transações",
+			"status_code": http.StatusInternalServerError,
+		})
+		return
+	}
+
+	queryParams := c.Request.URL.Query()
+
+	if len(queryParams) == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "É necessário passar ao manos um parâmetro de query string via URL",
+		})
+		return
+	}
+
+	var transactions []transaction
+	var filteredTransactions []transaction
+
+	json.Unmarshal(data, &transactions)
+
+	var (
+		idValue              = c.Query("id")
+		transactionCodeValue = c.Query("transaction_code")
+		currencyValue        = c.Query("currency")
+		senderValue          = c.Query("sender")
+		receiverValue        = c.Query("receiver")
+	)
+
+	var (
+		convertedId, errId                   = strconv.Atoi(idValue)
+		convertedTransactionCodeValue, errTc = strconv.Atoi(transactionCodeValue)
+	)
+
+	if errId != nil {
+		convertedId = 0
+	}
+
+	if errTc != nil {
+		convertedTransactionCodeValue = 0
+	}
+
+	for _, anyTransaction := range transactions {
+		if anyTransaction.TransactionCode == convertedTransactionCodeValue ||
+			anyTransaction.Id == convertedId ||
+			anyTransaction.Receiver == receiverValue ||
+			anyTransaction.Sender == senderValue ||
+			anyTransaction.Currency == currencyValue {
+			filteredTransactions = append(filteredTransactions, anyTransaction)
+		}
+		fmt.Println(idValue == string(anyTransaction.Id))
+	}
+
+	if len(filteredTransactions) == 0 {
+		// Retornando um array vazio em Golang JSON: https://stackoverflow.com/questions/56200925/return-an-empty-array-instead-of-null-with-golang-for-json-return-with-gin
+		c.JSON(http.StatusNotFound, make([]string, 0))
+		return
+	}
+
+	c.JSON(200, filteredTransactions)
+	return
+}
+
+func GetAll(c *gin.Context) {
+	data, err := ioutil.ReadFile("transactions.json")
 
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
@@ -51,5 +119,6 @@ func main() {
 	// https://stackoverflow.com/questions/39489175/how-to-define-a-go-gin-route-with-an-id-in-the-middle
 	req.GET("/hi/:name", helloWorld)
 	req.GET("/transactions", GetAll)
+	req.GET("/search", QueryStringFilter)
 	req.Run()
 }
